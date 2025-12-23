20d49ba6b0858cf538eef9175946ce70a510e72a
kurt.bryant <BCArpilleda@gmail.com>
2025-12-23 15:00:57 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'webview.dart';
+import 'phorjapan.dart';
+import 'japanFolder/webviewJP.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:unique_identifier/unique_identifier.dart';
+import 'api_service.dart';
+import 'japanFolder/api_serviceJP.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const platform = MethodChannel('com.example.countdown_timer/volume');
+  try {
+    await platform.invokeMethod('setMaxVolume');
+  } catch (e) {
+    print("Error setting volume: $e");
+  }
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? phOrJp = prefs.getString('phorjp');
+  String? deviceId = await UniqueIdentifier.serial;
+  ApiService.setupHttpOverrides();
+  ApiServiceJP.setupHttpOverrides();
+  String initialRoute = '/phorjapan'; // Default route
+
+  if (phOrJp == null) {
+    initialRoute = '/phorjapan';
+  } else if (deviceId != null) {
+    try {
+      dynamic response;
+
+      if (phOrJp == "ph") {
+        final apiService = ApiService();
+        response = await apiService.checkDeviceId(deviceId);
+      } else if (phOrJp == "jp") {
+        final apiServiceJP = ApiServiceJP();
+        response = await apiServiceJP.checkDeviceId(deviceId);
+      }
+
+      if (response['success'] == true) {
+        if (phOrJp == "ph") {
+          initialRoute = '/webView';
+        } else if (phOrJp == "jp") {
+          initialRoute = '/webViewJP';
+        }
+      }
+    } catch (e) {
+      print("Error checking device ID: $e");
+    }
+  }
+
+  runApp(MyApp(initialRoute: initialRoute));
+}
+
+class MyApp extends StatelessWidget {
+  final String initialRoute;
+
+  MyApp({required this.initialRoute});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IT JOB ORDER NOTIF',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: initialRoute,
+      routes: {
+        '/phorjapan': (context) => PhOrJpScreen(),
+        '/webView': (context) => SoftwareWebViewScreen(linkID: 28),
+        '/webViewJP': (context) => SoftwareWebViewScreenJP(linkID: 28),
+      },
+    );
+  }
+}
